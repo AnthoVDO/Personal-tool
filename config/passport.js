b581310c636c2fdbb39c621b8f3d2d849706b000
Anthony Van Den Ostende <anthony.vdo@hotmail.be>
2021-08-26 11:25:08 +0200

    I continued to work on the authentication

--- config/passport.js
+++ config/passport.js
@@ -4,3 +4,26 @@ import connection from "./database";
 
 LocalStrategy = LocalStrategy.Strategy;
 const User = connection.models.User;
+
+const strategy = new LocalStrategy((username, password, cb) => {
+
+    User.findOne({ username: usernamer }, (err, user) => {
+        if (err) { return cb(err) }
+        if (!user) {
+            return cb(null, false, {
+                message: "Incorrect username."
+            })
+        }
+        if (!user.validPassword(password)) {
+            return cb(null, false, { message: "Incorrect password." });
+        } else {
+            return cb(null, user);
+        }
+    })
+
+});
+
+
+passport.use(
+
+);
--- controller/users.routes.js
+++ controller/users.routes.js
@@ -1,10 +1,78 @@
 import express from "express";
 import passport from "passport";
-import passwordUtils from "../config/passwordUtils.js";
+import validPassword from "../config/passwordUtils";
+import genPassword from "../config/passwordUtils";
+import connection from "../config/database";
+import route from "color-convert/route";
 
-
+const User = connection.models.User;
 const router = express.Router;
 
-router.route("/users").get();
+/* Login */
+
+router.route("/api/v1/user/login")
+    .post((req, res, next) => {
+
+    })
+    .get((req, res, next) => {
+        const form = '<h1>Login Page</h1><form method="POST" action="/login">\
+    Enter Username:<br><input type="text" name="username">\
+    <br>Enter Password:<br><input type="password" name="password">\
+    <br><br><input type="submit" value="Submit"></form>';
+
+        res.send(form);
+    })
+
+/* Register */
+
+router.route("/api/v1/user/register")
+    .post((req, res, next) => {
+
+    })
+    .get((req, res, next) => {
+        const form = '<h1>Register Page</h1><form method="post" action="register">\
+                    Enter Username:<br><input type="text" name="username">\
+                    <br>Enter Password:<br><input type="password" name="password">\
+                    <br><br><input type="submit" value="Submit"></form>';
+
+        res.send(form);
+    })
+
+/*protected route */
+
+router.route("/api/v1/user/protected-route")
+    .get((req, res, next) => {
+        if (req.isAuthenticated()) {
+            req.send('<h1>You are authenticated</h1><p><a href="/logout">Logout and reload</a></p>')
+        } else {
+            res.send('<h1>You are not authenticated</h1><p><a href="/login">Login</a></p>');
+        }
+    })
+
+/* log out */
+
+router.route("/api/v1/user/logout").get((req, res, next) => {
+    req.logout();
+    res.redirect("/protected-route");
+})
+
+/* success */
+
+router.route("/api/v1/user/login-success").get((req, res, next) => {
+    res.send('<p>You successfully logged in. --> <a href="/protected-route">Go to protected route</a></p>')
+})
+
+/* Faillure */
+
+router.route("/api/v1/user/login-faillure").get((req, res, next) => {
+    res.send("You entered the wrong password.");
+})
+
+/* Root */
+
+router.route("/api/v1/user")
+    .get((req, res, next) => {
+        res.send("<h1>Home</h1><p>Please <a href='/register'>register</a></p>")
+    })
 
 module.exports = router;
